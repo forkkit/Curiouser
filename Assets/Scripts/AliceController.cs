f1190247148dd56b423926969a49af1e4cf12b69
mattrossman <mrossman@umass.edu>
2020-01-17 11:58:41 -0500

    Save scene and clear some junk from script

--- Assets/Scripts/AliceController.cs
+++ Assets/Scripts/AliceController.cs
@@ -3,17 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[System.Serializable] // This line allows the struct to appear in inspector
-public struct CollisionHandler {
-    public string tag;
-    public UnityEvent handler;
-}
-
 
 public class AliceController : MonoBehaviour
 {
-    public List<CollisionHandler> handlers;
-
     // Start is called before the first frame update
     void Start()
     {
