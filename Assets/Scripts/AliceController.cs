c742d3ba9179b419fe352ae7617a995fbe99b4c0
mattrossman <mrossman@umass.edu>
2020-01-17 11:57:16 -0500

    Add progressive growing and shrinking

--- Assets/Scripts/AliceController.cs
+++ Assets/Scripts/AliceController.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable] // This line allows the struct to appear in inspector
+public struct CollisionHandler {
+    public string tag;
+    public UnityEvent handler;
+}
+
 
 public class AliceController : MonoBehaviour
 {
+    public List<CollisionHandler> handlers;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +26,16 @@ public class AliceController : MonoBehaviour
         
     }
     
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerStay(Collider other) 
     {
+        const float scaleSpeed = 0.01f;
         if (other.gameObject.CompareTag("Food"))
         {
-            other.gameObject.SetActive(false);
+            this.transform.localScale += Vector3.one * scaleSpeed;
+        }
+        else if (other.gameObject.CompareTag("Drink"))
+        {
+            this.transform.localScale -= Vector3.one * scaleSpeed;
         }
     }
 }
