c09d2790dd44783ba3fd0e95115c34f75848f847
mattrossman <mrossman@umass.edu>
2020-01-17 16:20:15 -0500

    Add basic door open, add dummy char model

--- Assets/Scripts/AliceController.cs
+++ Assets/Scripts/AliceController.cs
@@ -6,6 +6,11 @@ using UnityEngine.Events;
 
 public class AliceController : MonoBehaviour
 {
+    float heightMin = 0.38f;
+    float heightMax = 2.75f;
+    public float height {
+        get { return (this.transform.localScale.magnitude - heightMin) / (heightMax - heightMin); }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +23,31 @@ public class AliceController : MonoBehaviour
         
     }
     
+    Vector3 clampVector3(Vector3 input, float min, float max) {
+        // Made this cause Vector3.ClampMagnitude only takes a maximum value
+        return new Vector3(
+            Mathf.Clamp(input.x, min, max),
+            Mathf.Clamp(input.y, min, max),
+            Mathf.Clamp(input.z, min, max)
+        );
+    }
+
     void OnTriggerStay(Collider other) 
     {
         const float scaleSpeed = 0.01f;
         if (other.gameObject.CompareTag("Food"))
         {
-            this.transform.localScale += Vector3.one * scaleSpeed;
+            this.transform.localScale = clampVector3(
+                this.transform.localScale + Vector3.one * scaleSpeed,
+                this.heightMin,
+                this.heightMax);
         }
         else if (other.gameObject.CompareTag("Drink"))
         {
-            this.transform.localScale -= Vector3.one * scaleSpeed;
+            this.transform.localScale = clampVector3(
+                this.transform.localScale - Vector3.one * scaleSpeed,
+                this.heightMin,
+                this.heightMax);
         }
     }
 }
---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DoorController : MonoBehaviour
+{
+    Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the Animator attached to the GameObject you are intending to animate.
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Key"))
+        {
+            Debug.Log("Used key!");
+            m_Animator.SetTrigger("Open");
+        }
+    }
+}
