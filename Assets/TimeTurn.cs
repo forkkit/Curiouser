ef25afd9dbfb9e1888bd208663f6a35e9210efef
vgupta24 <vikram.a.gupta@gmail.com>
2020-01-18 23:16:58 -0600

    Add pocketwatch

---
+++ Assets/TimeTurn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTurn : MonoBehaviour
+{
+    public bool turn = false;
+
+    public float speed = 1f;
+
+    public Transform pivot;
+    Vector3 rotation = new Vector3(0, -1, 0);
+
+    void Update()
+    {
+        if(!turn)
+            return;
+
+        pivot.Rotate(rotation * speed, Space.Self);
+    }
+}
